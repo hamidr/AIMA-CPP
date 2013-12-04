1f5c805192813cd7117a5abad44fdae47bbf9c64
Hamidr <n3gb0y@gmail.com>
2013-12-04 19:50:18 +0000

    also ran some tests to see how to works :)

--- node_test.cpp
+++ node_test.cpp
@@ -5,6 +5,7 @@
 
 using namespace AI;
 
+
 struct IntProblem : public Problem<int>
 {
   	IntProblem()
@@ -15,26 +16,103 @@ struct IntProblem : public Problem<int>
 		getRoot().addLeaf(getRoot());
 	}
 
-	void watch(const int &n) const override {
-		printf("%d\n",n);
-	}
 
-  	bool isGoal (const int & n) const override {
-		if ( n == 3 )
+  	bool isGoal (const int & value) const override {
+		if ( value == 3 )
 			return true;
 		return false;
 	}
 };
 
+struct TestProblem : public Problem<int>
+{
+  	TestProblem()
+	: Problem(0)
+	{}
+
+	virtual leafs_list successors(node_ptr &state) const override 
+	{
+		static int j = 0;
+		++j;
+		leafs_list eles;
+
+		static int k = 0;
+		++k;
+
+		if ( k >= 15 ) 
+			return eles;
+			
+
+		for(int i = j; i < j+2; ++i)
+			eles.push_back(makeNode(state->getState()+i, state));
+
+		return eles;
+
+	}
+
+  	bool isGoal (const int & value) const override {
+		if( value == 104 )
+			return true;
+		return false;
+	}
+};
+
+struct AlphabetProblem : public Problem<char>
+{
+	AlphabetProblem()
+	: Problem('A') 
+	{
+		auto &b = getRoot().addLeaf('B');
+			auto &d = b.addLeaf('D');
+				auto &h = d.addLeaf('H');
+				auto &i = d.addLeaf('I');
+			auto &e = b.addLeaf('E');
+				auto &j = e.addLeaf('J');
+				auto &k = e.addLeaf('K');
+
+		auto &c = getRoot().addLeaf('C');
+			auto &f = c.addLeaf('F');
+				auto &l = f.addLeaf('L');
+				auto &m = f.addLeaf('M');
+			auto &g = c.addLeaf('G');
+				auto &n = g.addLeaf('N');
+				auto &o = g.addLeaf('O');
+	}
+
+  	bool isGoal (const char & n) const override {
+		if ( n == 'H' )
+			return true;
+		return false;
+	}
+};
+
+template<typename T, typename F>
+void mapToRoot(const shared_ptr<Node<T>> &n, F f)
+{
+	if (!n) 
+		return;
+
+	mapToRoot(n->getParent(), f);
+	f(n->getState());
+}
 
 
 int main()
 {
-	DFGS(IntProblem());
-
-	printf("\n");
+	//DFGS(IntProblem());
+	//BFTS(IntProblem());
+	//DFGS(AlphabetProblem());
+	//
 	
-	BFTS(IntProblem());
+	// auto bar = DFGS(AlphabetProblem());
+
+	
+	auto ack = DFGS(TestProblem());
+	mapToRoot(ack, [](const int &n) {
+		std::cout << n << " ";
+	});
+	std::cout << std::endl;
+
 
 	return 0;
 }
